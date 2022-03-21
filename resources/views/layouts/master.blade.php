cda85876853df62cb7bf95f027417ab80a578c88
Rajiv Verma <rajiv@webguy.in>
2022-03-21 14:12:16 +0530

    Initial Commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8" />
+    <title> @yield('title') | Department of Health, Govt. of Meghalaya</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="ODAS Data Sync Dashboard" name="description" />
+    <meta content="Rajiv Verma" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico') }}">
+    @include('layouts.head-css')
+</head>
+
+@section('body')
+    <body data-sidebar="dark">
+@show
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+        @include('layouts.topbar')
+        @include('layouts.sidebar')
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+            <div class="page-content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+                <!-- container-fluid -->
+            </div>
+            <!-- End Page-content -->
+            @include('layouts.footer')
+        </div>
+        <!-- end main content-->
+    </div>
+    <!-- END layout-wrapper -->
+
+    <!-- Right Sidebar -->
+    {{-- @include('layouts.right-sidebar') --}}
+    <!-- /Right-bar -->
+
+    <!-- JAVASCRIPT -->
+    @include('layouts.vendor-scripts')
+</body>
+
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,37 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title" key="t-menu">@lang('translation.Menu')</li>
+
+                {{-- <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i><span class="badge rounded-pill bg-info float-end">04</span>
+                        <span key="t-dashboards">@lang('translation.Dashboards')</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="index" key="t-default">@lang('translation.Default')</a></li>
+                        <li><a href="dashboard-saas" key="t-saas">@lang('translation.Saas')</a></li>
+                        <li><a href="dashboard-crypto" key="t-crypto">@lang('translation.Crypto')</a></li>
+                        <li><a href="dashboard-blog" key="t-blog">@lang('translation.Blog')</a></li>
+                    </ul>
+                </li> --}}
+
+                <li>
+                    <a href="{{route('dashboard')}}" class="waves-effects" target="_blank">
+                        <i class="bx bx-home-circle"></i>
+                        <span key="Sync">@lang('translation.Dashboards')</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
