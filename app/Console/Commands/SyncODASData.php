f8476360e6afde05d96b42aef37f0ce397ec0dcb
Rajiv Verma <rajiv@webguy.in>
2022-04-21 13:35:32 +0530

    Added SyncData command

---
+++ app/Console/Commands/SyncODASData.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class SyncODASData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Syncs all Oxygen data from Google Sheet To the ODAS API';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Log::debug('SyncaData command fired');
+    }
+}
