f5c2e7cc2f30e1a88e963e0ebe8a201221a71f51
Rajiv Verma <rajiv@webguy.in>
2022-05-24 14:34:17 +0530

    added nullable DOB & avatar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->date('dob');
-            $table->string('avatar');
+            $table->date('dob')->nullable();
+            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
